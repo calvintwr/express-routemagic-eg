0e96376defe7d714ab86c4eccf065ffe9ac50d50
calvintwr <calvin14@gmail.com>
2020-05-30 11:23:17 +0800

    [update] magic v2.0.0

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const app = express()
 const magic = require('express-routemagic')
 
 //Basic usage
-magic.use(app, __dirname)
+//magic.use(app)
 
 //More options
 
-// magic.use(app,  __dirname, {
-//     printRoutes: true,
-//     routeFolder: 'routes',
-//     ignoreSuffix: ['_bak'],
-//     allowSameName: true,
-//     debug: function(msg) { console.log('\x1b[36m%s\x1b[0m', `From Magic: ${msg}`); } // outputs cyan console.log
-// })
+magic.use(app, {
+    logMapping: true,
+    routesFolder: 'routes',
+    invokerPath: __dirname,
+    ignoreSuffix: ['_bak'],
+    allowSameName: true,
+    debug: function(msg) { console.log('\x1b[36m%s\x1b[0m', `Magic: ${msg}`); } // outputs cyan console.log
+})
 module.exports = app
